92c96034752f062e4b39a45932776a919ded9563
Yury Moladau <yurymolodov@gmail.com>
2021-09-21 11:56:28 +0300

    feat: add copy legend value

--- app/vmui/packages/vmui/src/components/Legend/Legend.tsx
+++ app/vmui/packages/vmui/src/components/Legend/Legend.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from "react";
-import {Typography} from "@material-ui/core";
+import {Typography, Tooltip} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 
 export interface LegendItem {
@@ -17,14 +17,41 @@ export interface LegendProps {
 
 const useStyles = makeStyles({
   legendWrapper: {
-    display: "grid",
-    gridGap: "5px",
-    width: "100%",
+    display: "inline-grid",
+    width: "auto",
     gridTemplateColumns: "repeat(auto-fit)", // experiments like repeat(auto-fit, minmax(200px , auto)) may reduce size but readability as well
     gridColumnGap: ".5em",
-    paddingLeft: "8px"
+  },
+  legendSwatch: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "22px",
+    height: "12px",
+    marginRight: "8px",
+    border: "1px solid rgba(0, 0, 0, 0.1)",
+  },
+  legendItem: {
+    display: "flex",
+    alignItems: "center",
+    padding: "3px",
+    transition: "0.2s",
+    "&:hover": {
+      background: "rgba(0, 0, 0, 0.15)",
+    },
+  },
+  legendData: {
+    color: "#000",
+    transition: "0.2s",
+    "&:hover": {
+      color: "#3F51B5",
+    },
   }
 });
+
+const copyValue = async (key: string, value: string) => {
+  await navigator.clipboard.writeText(`${key}="${value}"`);
+};
 
 export const Legend: FC<LegendProps> = ({labels, onClick}) => {
   const classes = useStyles();
@@ -32,30 +59,27 @@ export const Legend: FC<LegendProps> = ({labels, onClick}) => {
   return <div>
     <div className={classes.legendWrapper}>
       {labels.map((legendItem: LegendItem) =>
-        <div key={legendItem.key} style={{
-          display: "flex",
-          alignItems: "center",
-          opacity: legendItem.checked ? 1 : 0.5,
-        }}>
+        <div key={legendItem.key} className={classes.legendItem}
+          style={{opacity: legendItem.checked ? 1 : 0.5}}>
           <div onClick={(event) => { onClick(event, legendItem); }}
-            style={{display: "flex", alignItems: "center", cursor: "pointer"}}>
-            <div style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "14px",
-              height: "14px",
-              marginRight: "4px",
-              border: "1px solid rgba(0, 0, 0, 0.1)",
-              backgroundColor: legendItem.color,
-            }}/>
+            style={{display: "flex", alignItems: "center", width: "100%", cursor: "pointer"}}>
+            <div className={classes.legendSwatch}
+              style={{backgroundColor: legendItem.color}}/>
             <Typography variant="body2">{legendItem.name}</Typography>
             <Typography variant="body2" style={{margin: "0 4px 0 8px"}}>{"{"}</Typography>
             {
               Object.keys(legendItem.labelData).map((labelKey) => (
-                <Typography variant="body2" key="labelKey" style={{marginRight: "4px"}}>
-                  <b>{labelKey}</b>:{legendItem.labelData[labelKey]}
-                </Typography>
+                <span className={classes.legendData} key={labelKey}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    copyValue(labelKey, legendItem.labelData[labelKey]);
+                  }}>
+                  <Tooltip title={`Copy '${labelKey}="${legendItem.labelData[labelKey]}"'`}>
+                    <Typography variant="body2" style={{marginRight: "4px"}}>
+                      <b>{labelKey}</b>:{legendItem.labelData[labelKey]}
+                    </Typography>
+                  </Tooltip>
+                </span>
               ))
             }
             <Typography variant="body2">{"}"}</Typography>
